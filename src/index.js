91ee54fca4208aa71ae156edaee38b562f2c3fb3
marina <marinafront@hotmail.fr>
2018-05-31 15:02:47 +0200

    instalation du routage (début)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//rooter
+import {BrowserRouter, Match, Miss} from 'react-router-dom';
 import App from './App';
+import NotFound from './composants/NotFound';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () =>{
+    return(
+        <BrowserRouter>
+            <div>
+                <Match exacty pattern="/" component={App}/>
+
+                <Miss component={NotFound}/>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
