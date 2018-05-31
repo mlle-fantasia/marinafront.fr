ba6665227dc047525502be7a13996f7db14899d5
marina <marinafront@hotmail.fr>
2018-05-31 13:14:43 +0200

    test instalation d'une galerie d'images

--- src/composants/pageRealisation/RealisationsPage.js
+++ src/composants/pageRealisation/RealisationsPage.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-
-import './RealisationsPage.css'
-import '@ladjs/bootstrap-social/bootstrap-social.css'
-import 'font-awesome/css/font-awesome.css'
+import Lightbox from 'react-images';
+import './RealisationsPage.css';
+import '@ladjs/bootstrap-social/bootstrap-social.css';
+import 'font-awesome/css/font-awesome.css';
 import {Grid, Row, Col} from 'react-bootstrap';
-
+import imprimEcranEx from '../images/imprimEcranEx.jpg';
+import diagramCasUtilisationexemples from '../images/diagramCasUtilisationexemples.jpg';
 import axios from 'axios';
 
 const API = "http://api-marinafront";
@@ -15,6 +16,7 @@ class RealisationsPage extends Component {
     state = {
         realisations: [],
         article: [],
+        images:[],
         articleDemande : '',
 
     }
@@ -23,12 +25,14 @@ class RealisationsPage extends Component {
     componentDidMount() {
         axios.get(API + "/realisation-article.php").then((response) => {
             if (response.data.error) {
+
                 console.log("tu as une erreur");
                 return true;
             }
-            const realisations = response.data.payload;
+            const realisations = response.data.payloadArticle;
             this.setState({realisations});
             console.log(realisations);
+
         });
     };
 
@@ -38,20 +42,31 @@ class RealisationsPage extends Component {
       };*/
 
     handleClickeUneRea(id) {
-        const {article, realisations, articleDemande} = this.state;
+        const {} = this.state;
         axios.get(API + "/realisation-article.php?id=" + id).then((response) => {
             if (response.data.error) {
                 console.log("tu as une erreur");
                 return true;
             }
-
-            let article = response.data.payload;
+            let article = response.data.payloadArticle;
             this.setState({article});
 
             let idArticleDemande = id;
             this.setState({articleDemande : idArticleDemande});
             console.log(id);
         });
+
+            axios.get(API + "/realisation-images.php?id=" + id).then((response) => {
+                if (response.data.error) {
+                    console.log("tu as une erreur");
+                    return true;
+                }
+                let newImages = response.data.payloadImages;
+                this.setState({images : newImages});
+                console.log(newImages);
+        });
+
+
     }
 
 
@@ -61,16 +76,54 @@ class RealisationsPage extends Component {
 
 
     render() {
-        const {realisations, article, articleDemande} = this.state;
+        const {realisations, article,images, articleDemande} = this.state;
 
+
+            const imagestest = [
+                {
+                    src: '../images/imprimEcranEx.jpg',
+                    title: "captures d'écran",
+                    description: 'image description'
+                },
+                {
+                    src: '../images/diagramCasUtilisationexemples.jpg',
+                    title: "captures d'écran",
+                    description: 'image description'
+                }
+            ];
+
+
+            console.log(imagestest);
+        /*const listeImages =images.map((object, i) =>
+            <div key={i}>
+
+                <Lightbox
+                    images={[
+                        { src: '{object.image}' },
+
+                    ]}
+                    isOpen={this.state.lightboxIsOpen}
+                    onClickPrev={this.gotoPrevious}
+                    onClickNext={this.gotoNext}
+                    onClose={this.closeLightbox}
+                />
+            </div>);
+
+        /*const listeImage = images.map((object, i) =>
+            <div key={i}>
+
+                    <div className="uneImage" onClick={() => {this.handleClickeUneImage()}}>
+                        <img src={object.image} alt={object.alt}/>
+                    </div>
+            </div>
+        );*/
 
         let listeAside = realisations.map((object, i) => {
-
-            if (object.id != articleDemande) {
+            if (object.id !== articleDemande) {
                 return (
                     <div key={i}>
                         <a>
-                            <div className="texte" onClick={() => {
+                            <div className="" onClick={() => {
                                 this.handleClickeUneRea(object.id)
                             }}>{object.titre}</div>
                         </a>
@@ -80,9 +133,7 @@ class RealisationsPage extends Component {
         });
 
 
-        console.log(listeAside);
-
-        const resultat = article.length == 0 ? (
+        const resultat = article.length === 0 ? (
             <Grid className="realisation">
                 <Row>
                     <Col xs={12} md={6}>
@@ -109,23 +160,26 @@ class RealisationsPage extends Component {
                         <h2>
                             <div className="petitTitre">{article.titre}</div>
                         </h2>
-                    </Col>
-                </Row>
-                <Row>
-
-                    <Col xs={12} sm={8} md={9} className="margin">
                         <div>{article.contenu}</div>
-                        <button onClick={() => {
+                        <button className="btnRetour" onClick={() => {
                             this.retour()
-                        }}>retour
+                            }}>retour
                         </button>
+                        <div className="listeAside">
+                            <h3 className="asideTitre">Les autres réalisations</h3>
+                            {listeAside}
+                        </div>
                     </Col>
-                    <Col xs={12} sm={4} md={3} className="aside">
-                    <h3 className="asideTitre">Les réalisations</h3>
-                        {listeAside}
+                    <Col xs={12} sm={4} md={3} className="images">
+                        <h3 className="galerieTitre">Les images</h3>
+                        <Lightbox images={imagestest}
+                                  isOpen={this.state.lightboxIsOpen}
+                                  onClickPrev={this.gotoPrevious}
+                                  onClickNext={this.gotoNext}
+                                  onClose={this.closeLightbox}
+                        />
 
                     </Col>
-
                 </Row>
             </Grid>
 
